6ae5a8b46835d180bbab46b1002ff00d9ecf696a
SanjayDevTech <sureshsanjay805@gmail.com>
2021-09-07 10:22:48 +0530

    ViewBinding added

--- app/src/main/java/com/sanjaydevtech/pexelsgallery/MainActivity.kt
+++ app/src/main/java/com/sanjaydevtech/pexelsgallery/MainActivity.kt
@@ -2,10 +2,13 @@ package com.sanjaydevtech.pexelsgallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sanjaydevtech.pexelsgallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
